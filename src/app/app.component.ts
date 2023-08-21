e8d4d74fab5ca4bca597ba9958ccf48eb679af42
frigolin <frzrigolin@gmail.com>
2023-08-21 17:27:55 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-angular';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.gerarNumero();
+  }
+
+  // exemplo 01 - Binding Unidirecional
+  // Property Binding: propriedade gerada na classe e consumida no html (classe -> view)
+  //rnd : number = Math.random();
+  rnd! : number;  // rnd : number | undefined;
+  
+   // Event Binding: este método é executado através do evento click no html (view -> classe)
+  gerarNumero() : void {
+    this.rnd = Math.random();
+  }
+
+
+
 }
